76cb3129474ca4b0df8875255018c0999741d234
Nikolai Artemiev <26139730+n7a8@users.noreply.github.com>
2017-08-25 19:32:10 +1200

    Add list of models to DOM when a scene is loaded.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App {
     async loadScene(encoded) {
         this.scene = await Scene.fromJSONEncodableObj(this.pt.gl, JSON.parse(encoded));
         this.scene.uploadSceneData();
+        this.updateModelList();
     }
 
     render() {
@@ -18,23 +19,42 @@ class App {
         }
     }
 
+    updateModelList() {
+        const listElem    = document.querySelector("#scene-model-list");
+        const rowTemplate = document.querySelector("#scene-model-row-template").content;
+
+        let i = 0;
+        for(const [model, mat] of this.scene.models) {
+            const row = document.importNode(rowTemplate, true);
+
+            // TODO: assign sensible names
+            row.querySelector(".scene-model-name").innerText = `Model ${i++}`;
+
+            row.querySelector(".scene-model-position").innerText = model.position.array();
+            row.querySelector(".scene-model-size").innerText = model.size.array();
+
+            listElem.appendChild(row);
+        }
+    }
+
     run() {
-        document.getElementById("load-button").addEventListener("click", async () => {
+        document.querySelector("#render-output").appendChild(this.pt.gl.canvas);
+
+        document.querySelector("#load-button").addEventListener("click", async () => {
             const file = await loadFile();
             const fr = new FileReader();
             fr.onloadend = () => this.loadScene(fr.result);
             fr.readAsText(file);
         });
 
-        document.getElementById("save-button").addEventListener("click", () => {
-            const data = JSON.stringify(this.pt.scene.toJSONEncodableObj());
-            saveFile(data, "scene.json");
+        document.querySelector("#save-button").addEventListener("click", () => {
+            if(this.scene) {
+                const data = JSON.stringify(this.scene.toJSONEncodableObj());
+                saveFile(data, "scene.json");
+            }
         });
 
-        document.getElementById("render-button")
-            .addEventListener("click", () => this.render());
-
-        document.getElementById("render-output").appendChild(this.pt.gl.canvas);
+        document.querySelector("#render-button").addEventListener("click", () => this.render());
     }
 }
 
